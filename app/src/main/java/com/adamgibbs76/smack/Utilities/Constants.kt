934836dce5da229095983237ef94787490726c83
adamgibbs <adamcgibbs76@gmail.com>
2018-07-08 15:00:30 +0930

    lesson-11

--- app/src/main/java/com/adamgibbs76/smack/Utilities/Constants.kt
+++ app/src/main/java/com/adamgibbs76/smack/Utilities/Constants.kt
@@ -3,3 +3,4 @@ package com.adamgibbs76.smack.Utilities
 //const val BASE_URL = "https://chattygibbschat.herokuapp.com/v1/"
 const val BASE_URL = "http://10.0.2.2:3005/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
+const val URL_LOGIN = "${BASE_URL}account/login"
