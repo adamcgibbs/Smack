f26ce65cc95e1ab240c8af93195f647f9d0d354a
adamgibbs <adamcgibbs76@gmail.com>
2018-07-08 20:09:17 +0930

    lesson-14

--- app/src/main/java/com/adamgibbs76/smack/Services/UserDataService.kt
+++ app/src/main/java/com/adamgibbs76/smack/Services/UserDataService.kt
@@ -1,5 +1,8 @@
 package com.adamgibbs76.smack.Services
 
+import android.graphics.Color
+import java.util.*
+
 object UserDataService {
 
     var id = ""
@@ -8,5 +11,35 @@ object UserDataService {
     var email = ""
     var name = ""
 
+    fun returnAvatarColor(components: String) : Int {
+        //[0.792156862745098, 0.8588235294117647, 0.0784313725490196, 1]
 
+        val strippedColor = components
+                .replace("[", "")
+                .replace("]", "")
+                .replace(",", "")
+        var r = 0
+        var g = 0
+        var b = 0
+
+        val scanner = Scanner(strippedColor)
+        if(scanner.hasNext()){
+            r = (scanner.nextDouble() * 255).toInt()
+            g = (scanner.nextDouble() * 255).toInt()
+            b = (scanner.nextDouble() * 255).toInt()
+        }
+
+        return Color.rgb(r,g,b)
+    }
+
+    fun logout() {
+        id = ""
+        avatarColor = ""
+        avatarName = ""
+        email = ""
+        name = ""
+        AuthService.authToken = ""
+        AuthService.userEmail = ""
+        AuthService.isLoggedIn = false
+    }
 }
