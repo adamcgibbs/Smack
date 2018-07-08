6c9f77bd407d5d6cb1d91dc3b4351bd3bba75e56
adamgibbs <adamcgibbs76@gmail.com>
2018-07-08 15:42:49 +0930

    lesson-12

--- app/src/main/java/com/adamgibbs76/smack/Services/AuthService.kt
+++ app/src/main/java/com/adamgibbs76/smack/Services/AuthService.kt
@@ -2,6 +2,7 @@ package com.adamgibbs76.smack.Services
 
 import android.content.Context
 import android.util.Log
+import com.adamgibbs76.smack.Utilities.URL_CREATEUSER
 import com.adamgibbs76.smack.Utilities.URL_LOGIN
 import com.adamgibbs76.smack.Utilities.URL_REGISTER
 import com.android.volley.Response
@@ -81,4 +82,48 @@ object AuthService {
         Volley.newRequestQueue(context).add(loginRequest)
     }
 
+    fun createUser(context: Context, name: String, email: String, avatarName: String, avatarColor: String, complete: (Boolean) -> Unit){
+
+        val jsonBody = JSONObject()
+        jsonBody.put("name", name)
+        jsonBody.put("email", email)
+        jsonBody.put("avatarName", avatarName)
+        jsonBody.put("avatarColor", avatarColor)
+        val requestBody = jsonBody.toString()
+
+        val createRequest = object : JsonObjectRequest(Method.POST, URL_CREATEUSER, null, Response.Listener {response ->
+
+            try {
+                UserDataService.name = response.getString("name")
+                UserDataService.email = response.getString("email")
+                UserDataService.avatarName = response.getString("avatarName")
+                UserDataService.avatarColor = response.getString("avatarColor")
+                UserDataService.id = response.getString("_id")
+                complete(true)
+
+            } catch (e: JSONException){
+                Log.d("JSON", "EXC " + e.localizedMessage)
+            }
+
+        }, Response.ErrorListener {error ->
+            Log.d("ERROR", "Could not add user: $error")
+            complete(false)
+        }) {
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getBody(): ByteArray {
+                return requestBody.toByteArray()
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers.put("Authorization", "Bearer $authToken")
+                return headers
+            }
+        }
+
+        Volley.newRequestQueue(context).add(createRequest)
+    }
 }
---
+++ app/src/main/java/com/adamgibbs76/smack/Services/UserDataService.kt
@@ -0,0 +1,12 @@
+package com.adamgibbs76.smack.Services
+
+object UserDataService {
+
+    var id = ""
+    var avatarColor = ""
+    var avatarName = ""
+    var email = ""
+    var name = ""
+
+
+}
