1758348fc85a8e229c44ef2750822152881d61a9
adamgibbs <adamcgibbs76@gmail.com>
2018-07-09 22:21:19 +0930

    lesson-19

--- app/src/main/java/com/adamgibbs76/smack/Utilities/Constants.kt
+++ app/src/main/java/com/adamgibbs76/smack/Utilities/Constants.kt
@@ -7,7 +7,8 @@ const val SOCKET_URL = "https://chattygibbschat.herokuapp.com/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
 const val URL_CREATEUSER = "${BASE_URL}user/add"
-const val URL_GET_USER = "${BASE_URL}/user/byEmail/"
+const val URL_GET_USER = "${BASE_URL}user/byEmail/"
+const val URL_GET_CHANNELS = "${BASE_URL}channel"
 
 // Broadcast constants
 const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
